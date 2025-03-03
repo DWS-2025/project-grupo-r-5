47e0e76d22080c17101bf7ab3a7bc396dfb5b4f5
alexone522 <aterrazas522@gmail.com>
2025-03-03 22:21:14 +0100

    Remove GroupalClass from admin panel is completed

--- src/main/java/es/codeurjc/web/Controller/AdminController.java
+++ src/main/java/es/codeurjc/web/Controller/AdminController.java
@@ -1,5 +1,7 @@
 package es.codeurjc.web.Controller;
 
+import es.codeurjc.web.Model.ClassUser;
+import es.codeurjc.web.Model.GroupClass;
 import org.springframework.ui.Model;
 import es.codeurjc.web.Service.GroupClassService;
 import es.codeurjc.web.Service.PostService;
@@ -7,9 +9,16 @@ import es.codeurjc.web.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class AdminController {
+    @Autowired
+    private UserService userService;
     @Autowired
     private PostService postService;
     @Autowired
@@ -37,6 +46,25 @@ public class AdminController {
     public String showGroupClasses(Model model) {
         model.addAttribute("GroupClasses", groupClassService.findAll());
         return "adminGroupClasses";
+    }
+    @GetMapping("/admin/groupClasses/delete-{id}")
+    public String deleteGroupClass(@PathVariable long id, Model model) {
+        model.addAttribute("groupClass",groupClassService.findById(id));
+        return "adminRemoveClassConfirmation";
+    }
+    @PostMapping("/admin/groupClasses/delete-{id}")
+    public String deleteGroupClassConfirmed(@PathVariable long id) {
+        GroupClass groupClass = groupClassService.findById(id);
+        List<ClassUser> usersList= groupClass.getUsersList();
+        if(!usersList.isEmpty()){
+            for (ClassUser user:usersList){
+                long userId = user.getUserid();
+                userService.removeGroupClass(id,userId);
+                groupClassService.removeUser(id,userId);
+            }
+        }
+        groupClassService.delete(id);
+        return "redirect:/admin/groupClasses";
     }
 
 }
