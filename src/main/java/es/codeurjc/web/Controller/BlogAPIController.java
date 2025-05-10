66b98a4757d44dc873b84f794720ebb7a48c549e
“Lnita” <e.arcaz.2022@alumnos.urjc.es>
2025-05-10 21:11:45 +0200

    Implementing basic query operations for the API

--- src/main/java/es/codeurjc/web/Controller/BlogAPIController.java
+++ src/main/java/es/codeurjc/web/Controller/BlogAPIController.java
@@ -1,13 +1,21 @@
 package es.codeurjc.web.Controller;
 
+import es.codeurjc.web.Dto.PostDTO;
 import es.codeurjc.web.Service.ImageService;
 import es.codeurjc.web.Service.PostService;
 import es.codeurjc.web.Service.UserService;
 import es.codeurjc.web.Service.ValidateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Collection;
 
 @RestController
+@RequestMapping("/posts")
 public class BlogAPIController {
     @Autowired
     private UserService userService;
@@ -17,6 +25,17 @@ public class BlogAPIController {
     private ImageService imageService;
     @Autowired
     private ValidateService validateService;
+
+    @GetMapping("/")
+    public Page<PostDTO> getPosts(Pageable page) {
+        return postService.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public PostDTO getPost(long id) {
+        return postService.getPost(id);
+    }
+
 
 
 }
--- src/main/java/es/codeurjc/web/Controller/GroupClassAPIController.java
+++ src/main/java/es/codeurjc/web/Controller/GroupClassAPIController.java
@@ -1,12 +1,19 @@
 package es.codeurjc.web.Controller;
 
+import es.codeurjc.web.Dto.GroupClassBasicDTO;
+import es.codeurjc.web.Dto.GroupClassDTO;
 import es.codeurjc.web.Service.GroupClassService;
 import es.codeurjc.web.Service.UserService;
 import es.codeurjc.web.Service.ValidateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/groupClass")
 public class GroupClassAPIController {
     @Autowired
     private GroupClassService groupClassService;
@@ -16,5 +23,14 @@ public class GroupClassAPIController {
     private ValidateService validateService;
 
 
+    @GetMapping("/")
+    public Page<GroupClassBasicDTO> getGroupClasses(Pageable page) {
+        return groupClassService.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public GroupClassDTO getGroupClass(long id) {
+        return groupClassService.findById(id).orElseThrow();
+    }
 
 }
