a15dcc9916c5206097255fd7a489ad0f973527d5
“Lnita” <e.arcaz.2022@alumnos.urjc.es>
2025-06-14 19:39:51 +0200

    Correcting the Data Base initialization

---
+++ src/main/java/es/codeurjc/web/Service/BasicDataInitializer.java
@@ -0,0 +1,110 @@
+package es.codeurjc.web.Service;
+
+
+import es.codeurjc.web.Domain.ClassUser;
+import es.codeurjc.web.Domain.GroupClass;
+import es.codeurjc.web.Domain.Post;
+import es.codeurjc.web.Repositories.GroupClassRepository;
+import es.codeurjc.web.Repositories.PostRepository;
+import es.codeurjc.web.Repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Transactional
+@Service
+public class BasicDataInitializer {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private GroupClassRepository groupClassRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @PostConstruct
+    public void init() throws IOException {
+        //create users first:
+        ClassUser user1 = new ClassUser("Pepe");
+        ClassUser user2 = new ClassUser("Juan");
+        ClassUser user3 = new ClassUser("Yoshi");
+        ClassUser user4 = new ClassUser("Zelda");
+        ClassUser user5 = new ClassUser("Rufusberto");
+        ClassUser user6 = new ClassUser("Kirby");
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+        userRepository.save(user4);
+        userRepository.save(user5);
+        userRepository.save(user6);
+
+
+        //group classes next:
+        GroupClass class1 = new GroupClass("Advanced yoga", DayOfWeek.MONDAY, LocalTime.parse("10:00"), 60, "Professor A", 20);
+        GroupClass class2 = new GroupClass("Pilates", DayOfWeek.TUESDAY, LocalTime.parse("15:00"), 120, "Professor B", 15);
+        GroupClass class3 = new GroupClass("Zumba", DayOfWeek.WEDNESDAY, LocalTime.parse("12:00"), 90, "Professor C", 25);
+        GroupClass class4 = new GroupClass("CrossFit", DayOfWeek.THURSDAY, LocalTime.parse("18:00"), 45, "Professor D", 30);
+        GroupClass class5 = new GroupClass("Spinning", DayOfWeek.FRIDAY, LocalTime.parse("17:00"), 60, "Professor E", 20);
+        GroupClass class6 = new GroupClass("Aerobics", DayOfWeek.SATURDAY, LocalTime.parse("09:00"), 60, "Professor F", 25);
+        GroupClass class7 = new GroupClass("Aerobics", DayOfWeek.SATURDAY, LocalTime.parse("10:00"), 60, "Professor F", 25);
+
+        groupClassRepository.save(class1);
+        groupClassRepository.save(class2);
+        groupClassRepository.save(class3);
+        groupClassRepository.save(class4);
+        groupClassRepository.save(class5);
+        groupClassRepository.save(class6);
+        groupClassRepository.save(class7);
+
+        //Assign users to group classes:
+        userService.addGroupClass(class1.getClassid(), user1.getUserid());
+        userService.addGroupClass(class1.getClassid(), user2.getUserid());
+        userService.addGroupClass(class2.getClassid(), user3.getUserid());
+        userService.addGroupClass(class2.getClassid(), user4.getUserid());
+        userService.addGroupClass(class3.getClassid(), user5.getUserid());
+        userService.addGroupClass(class3.getClassid(), user6.getUserid());
+        userService.addGroupClass(class3.getClassid(), user1.getUserid());
+        userService.addGroupClass(class4.getClassid(), user2.getUserid());
+        userService.addGroupClass(class5.getClassid(), user3.getUserid());
+        userService.addGroupClass(class5.getClassid(), user4.getUserid());
+        userService.addGroupClass(class6.getClassid(), user5.getUserid());
+
+
+
+
+        //Then create posts:
+        Post post1 = new Post(user1, "Pedaleando al ritmo de la música", "¡Menuda clase de spinning!...");
+        Post post2 = new Post(user5, "Desconexión total en la clase de yoga", "Hoy probé la clase de yoga...");
+        Post post3 = new Post(user6, "Nunca había sudado tanto", "¡La clase de CrossFit de hoy fue brutal!...");
+        Post post4 = new Post(user5, "Bailar y entrenar al mismo tiempo", "Hoy fue mi primera clase de zumba y ¡me encantó!");
+        Post post5 = new Post(user1, "Energía al máximo", "No sabía que una clase de aerobics podía ser TAN intensa.");
+        Post post6 = new Post(user5, "Fortaleciendo el cuerpo con Pilates", "Hoy fui a mi primera clase de pilates y me sorprendió lo exigente que puede ser.");
+
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+        postRepository.save(post4);
+        postRepository.save(post5);
+        postRepository.save(post6);
+
+        //Assign users to posts:
+        userService.addPost(post1.getPostid(), user1.getUserid());
+        userService.addPost(post2.getPostid(), user5.getUserid());
+        userService.addPost(post3.getPostid(), user6.getUserid());
+        userService.addPost(post4.getPostid(), user5.getUserid());
+        userService.addPost(post5.getPostid(), user1.getUserid());
+        userService.addPost(post6.getPostid(), user5.getUserid());
+
+    }
+
+}
